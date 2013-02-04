d05e2795692c40561e453b90353bf7caeae15c81
Ranganath Atreya <daivajnanam@gmail.com>
2013-02-03 17:10:25 -0800

    [Added tests for the input method engine] [Fixed bugs in the input method engine]

---
+++ LipikaIMETest/LipikaIMEEngineTest.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LipikaIMEEngineTest : SenTestCase
+
+@end
---
+++ LipikaIMETest/LipikaIMESchemeTest.m
@@ -0,0 +1,61 @@
+#import "LipikaIMESchemeTest.h"
+#import "DJInputMethodScheme.h"
+#import "DJParseTreeNode.h"
+
+@implementation LipikaIMESchemeTest
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testHeaderParsing {
+    NSLog(@"%@", [[NSBundle mainBundle] bundlePath]);
+    DJInputMethodScheme* scheme = [[DJInputMethodScheme alloc] initWithSchemeFile:@"/Users/ratreya/workspace/Lipika_IME/LipikaIMETest/TestHappyCase.scm"];
+    STAssertTrue([@"1.0" isEqualTo:[scheme version]], @"Version numbers don't match");
+    STAssertTrue([@"Barahavat" isEqualTo:[scheme name]], @"Names don't match");
+    STAssertTrue([@"\\" isEqualTo:[scheme stopChar]], @"Stop Characters dos't match");
+    STAssertTrue([scheme usingClasses], @"Using Classes don't match");
+    STAssertTrue([@"{" isEqualToString:[scheme classOpenDelimiter]], @"Class open delimiters don't match");
+    STAssertTrue([@"}" isEqualToString:[scheme classCloseDelimiter]], @"Class close delimiters don't match");
+    STAssertTrue([@"*" isEqualToString:[scheme wildcard]], @"Wildcards don't match");
+}
+
+- (void)testClassParsing {
+    NSLog(@"%@", [[NSBundle mainBundle] bundlePath]);
+    DJInputMethodScheme* scheme = [[DJInputMethodScheme alloc] initWithSchemeFile:@"/Users/ratreya/workspace/Lipika_IME/LipikaIMETest/TestClassHappy.scm"];
+    STAssertTrue([@"test" isEqualToString:[scheme getClassNameForInput:@"c"]], @"Unexpected class name");
+    STAssertTrue([[scheme getClassForName:@"test"] count] == 3, @"Unexpected count of mappings");
+}
+
+- (void)testNestedClassParsing {
+    NSLog(@"%@", [[NSBundle mainBundle] bundlePath]);
+    DJInputMethodScheme* scheme = [[DJInputMethodScheme alloc] initWithSchemeFile:@"/Users/ratreya/workspace/Lipika_IME/LipikaIMETest/TestNestedClass.scm"];
+    STAssertTrue([@"test1" isEqualToString:[scheme getClassNameForInput:@"c"]], @"Unexpected class name");
+    STAssertTrue([@"test2" isEqualToString:[scheme getClassNameForInput:@"f"]], @"Unexpected class name");
+    STAssertTrue([[scheme getClassForName:@"test1"] count] == 3, @"Unexpected count of mappings");
+    STAssertTrue([[scheme getClassForName:@"test2"] count] == 2, @"Unexpected count of mappings");
+}
+
+- (void)testMappingParsing {
+    NSLog(@"%@", [[NSBundle mainBundle] bundlePath]);
+    DJInputMethodScheme* scheme = [[DJInputMethodScheme alloc] initWithSchemeFile:@"/Users/ratreya/workspace/Lipika_IME/LipikaIMETest/TestHappyCase.scm"];
+    NSString* output = [[[[[scheme parseTree] valueForKey:@"~"] next] valueForKey:@"j"] output];
+    STAssertTrue([output isEqualToString: @"ञ्"], @"Unexpected output");
+    output = [[[[[[[scheme parseTree] valueForKey:@"~"] next] valueForKey:@"j"] next] valueForKey:@"VowelSigns"] output];
+    STAssertTrue([output isEqualToString: @"ञ%@"], @"Unexpected output");
+}
+
+
+// Ignoring for now; @ symbol does not seem to work
+- (void)XXXtestSpecialCharacterParsing {
+    NSLog(@"%@", [[NSBundle mainBundle] bundlePath]);
+    DJInputMethodScheme* scheme = [[DJInputMethodScheme alloc] initWithSchemeFile:@"/Users/ratreya/workspace/Lipika_IME/LipikaIMETest/TestSpecialChars.scm"];
+    STAssertTrue(scheme != nil, @"Unable to parse special characters");
+}
+
+
+@end
