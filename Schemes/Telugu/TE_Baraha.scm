9c900b2f3010c8634c78a54409a989e78d5ecc73
Ranganath Atreya <daivajnanam@gmail.com>
2013-07-24 07:43:27 -0700

    [Added mappings for Telugu] [Fixed bug in Kannada ITRANS mappings]

---
+++ Schemes/Telugu/TE_Baraha.scm
@@ -0,0 +1,211 @@
+version: 1.0
+name: Baraha
+stop-char: \
+using classes
+class-delimiters: { }
+wildcard: *
+
+a అ
+aa ఆ
+A ఆ
+i ఇ
+ee ఈ
+I ఈ
+u ఉ
+oo ఊ
+U ఊ
+Ru ఋ
+RU ౠ
+~lu ఌ
+~lU ౡ
+e ఎ
+E ఏ
+ai ఐ
+o ఒ
+O ఓ
+au ఔ
+ou ఔ
+
+ka క
+kha ఖ
+Ka ఖ
+ga గ
+gha ఘ
+Ga ఘ
+~ga ఙ
+ca చ
+cha ఛ
+Ca ఛ
+ja జ
+jha ఝ
+Ja ఝ
+~ja ఞ
+Ta ట
+Tha ఠ
+Da డ
+Dha ఢ
+Na ణ
+ta త
+tha థ
+da ద
+dha ధ
+na న
+pa ప
+pha ఫ
+Pa ఫ
+ba బ
+bha భ
+Ba భ
+ma మ
+ya య
+ra ర
+rxa ఱ
+la ల
+va వ
+sha శ
+Sa శ
+Sha ష
+sa స
+ha హ
+La ళ
+
+class VowelSigns {
+  aa ా
+  A ా
+  i ి
+  ee ీ
+  I ీ
+  u ు
+  oo ూ
+  U ూ
+  Ru ృ
+  RU ౄ
+  ~lu ౢ
+  ~lU ౣ
+  e ె
+  E ే
+  ai ై
+  o ొ
+  O ో
+  au ౌ
+  ou ౌ
+}
+
+k క్
+kh ఖ్
+K ఖ్
+g గ్
+gh ఘ్
+G ఘ్
+~g ఙ్
+c చ్
+ch ఛ్
+C ఛ్
+j జ్
+jh ఝ్
+J ఝ్
+~j ఞ్
+T ట్
+Th ఠ్
+D డ్
+Dh ఢ్
+N ణ్
+t త్
+th థ్
+d ద్
+dh ధ్
+n న్
+p ప్
+ph ఫ్
+P ఫ్
+b బ్
+bh భ్
+B భ్
+m మ్
+y య్
+r ర్
+rx ఱ్
+l ల్
+v వ్
+sh శ్
+S శ్
+Sh ష్
+s స్
+h హ్
+L ళ్
+
+k{VowelSigns} క*
+kh{VowelSigns} ఖ*
+K{VowelSigns} ఖ*
+g{VowelSigns} గ*
+gh{VowelSigns} ఘ*
+G{VowelSigns} ఘ*
+~g{VowelSigns} ఙ*
+c{VowelSigns} చ*
+ch{VowelSigns} ఛ*
+C{VowelSigns} ఛ*
+j{VowelSigns} జ*
+jh{VowelSigns} ఝ*
+J{VowelSigns} ఝ*
+~j{VowelSigns} ఞ*
+T{VowelSigns} ట*
+Th{VowelSigns} ఠ*
+D{VowelSigns} డ*
+Dh{VowelSigns} ఢ*
+N{VowelSigns} ణ*
+t{VowelSigns} త*
+th{VowelSigns} థ*
+d{VowelSigns} ద*
+dh{VowelSigns} ధ*
+n{VowelSigns} న*
+p{VowelSigns} ప*
+ph{VowelSigns} ఫ*
+P{VowelSigns} ఫ*
+b{VowelSigns} బ*
+bh{VowelSigns} భ*
+B{VowelSigns} భ*
+m{VowelSigns} మ*
+y{VowelSigns} య*
+r{VowelSigns} ర*
+rx{VowelSigns} ఱ*
+l{VowelSigns} ల*
+v{VowelSigns} వ*
+sh{VowelSigns} శ*
+S{VowelSigns} శ*
+Sh{VowelSigns} ష*
+s{VowelSigns} స*
+h{VowelSigns} హ*
+L{VowelSigns} ళ*
+
+0 ౦
+1 ౧
+2 ౨
+3 ౩
+4 ౪
+5 ౫
+6 ౬
+7 ౭
+8 ౮
+9 ౯
+
+M ం
+H ః
+~M ఁ
+
+x ೱ
+f ೲ
+
+& ఽ
+| ।
+! ॥
+
+aum ॐ
+
+' ॑
+'' ᳚
+_ ॒
+
+^ ‍
+^^ ‌
+
+q ్
