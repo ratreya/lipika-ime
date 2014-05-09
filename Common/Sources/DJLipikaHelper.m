6e4ca6312b0f63246336317799e8baf98240b643
Ranganath Atreya <daivajnanam@gmail.com>
2014-05-09 16:21:01 -0700

    Fixed LipikaHelper to only have extern static functions

--- Common/Sources/DJLipikaHelper.m
+++ Common/Sources/DJLipikaHelper.m
@@ -9,21 +9,16 @@
 
 #import "DJLipikaHelper.h"
 
-@implementation DJLipikaHelper
-
 static NSRegularExpression *whitespaceExpression;
 
-+(void)initialize {
-    NSString *const whitespacePattern = @"^\\s+$";
-    NSError *error;
-    whitespaceExpression = [NSRegularExpression regularExpressionWithPattern:whitespacePattern options:0 error:&error];
-    if (error != nil) {
-        [NSException raise:@"Invalid class key regular expression" format:@"Regular expression error: %@", [error localizedDescription]];
-    }
-}
-
 BOOL isWhitespace(NSString *string) {
+    if (!whitespaceExpression) {
+        NSString *const whitespacePattern = @"^\\s+$";
+        NSError *error;
+        whitespaceExpression = [NSRegularExpression regularExpressionWithPattern:whitespacePattern options:0 error:&error];
+        if (error != nil) {
+            [NSException raise:@"Invalid class key regular expression" format:@"Regular expression error: %@", [error localizedDescription]];
+        }
+    }
     return [whitespaceExpression numberOfMatchesInString:string options:0 range:NSMakeRange(0, string.length)];
 }
-
-@end
