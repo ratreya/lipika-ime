c6f987844e1c3f111b03eddb527d0b75b7fc607a
Ranganath Atreya <daivajnanam@gmail.com>
2014-06-10 21:43:48 -0700

    Fixed to install without restart

--- Installer/Source/main.m
+++ Installer/Source/main.m
@@ -12,43 +12,66 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *currentUser = @"~/Library/Input Methods/LipikaIME.app";
-        NSString *allUsers = @"/Library/Input Methods/LipikaIME.app";
+        NSString *path = @"/Library/Input Methods/LipikaIME.app";
         NSString *bundleId = @"com.daivajnanam.inputmethod.LipikaIME";
         
-        NSString *path;
-        BOOL isDirectory;
-        if ([[NSFileManager defaultManager] fileExistsAtPath:currentUser isDirectory:&isDirectory]) {
-            path = currentUser;
-        }
-        else if ([[NSFileManager defaultManager] fileExistsAtPath:allUsers isDirectory:&isDirectory]) {
-            path = allUsers;
+        // Process command line arguments
+        NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+        // Defaults
+        BOOL remove = NO;
+        BOOL enable = NO;
+        BOOL select = NO;
+        if ([arguments count] != 1) {
+            if ([arguments[1] isEqualToString:@"--remove"]) remove = YES;
+            else if ([arguments[1] isEqualToString:@"--enable"]) enable = YES;
+            else if ([arguments[1] isEqualToString:@"--select"]) select = YES;
+            else {
+                printf("[ERROR] Unrecognized argument: %s\n", [arguments[1] UTF8String]);
+                return -1;
+            }
         }
         else {
-            NSLog(@"Unable to find LipikaIME at %@ or %@", currentUser, allUsers);
+            printf("[ERROR] You should specify one of --remove or --enable or --select\n");
             return -1;
         }
-        NSURL *location = [[NSURL alloc] initFileURLWithPath:path isDirectory:isDirectory];
-        OSStatus status = TISRegisterInputSource((__bridge CFURLRef)(location));
-        if (status == paramErr) {
-            NSLog(@"Unable to register input source at %@", location);
-            return -1;
+
+        OSStatus status;
+        if (enable) {
+            BOOL isDirectory;
+            NSURL *location = [[NSURL alloc] initFileURLWithPath:path isDirectory:isDirectory];
+            status = TISRegisterInputSource((__bridge CFURLRef)(location));
+            if (status == paramErr) {
+                printf("[ERROR] Unable to register input source at %s\n", [path UTF8String]);
+                return -1;
+            }
         }
-        NSDictionary *properties = [NSDictionary dictionaryWithObject:bundleId forKey(NSString *)kTISPropertyBundleID];
+        NSDictionary *properties = [NSDictionary dictionaryWithObject:bundleId forKey:(NSString *)kTISPropertyBundleID];
         NSArray *inputlist = (__bridge NSArray *)(TISCreateInputSourceList((__bridge CFDictionaryRef)(properties), YES));
         if (inputlist.count != 1) {
-            NSLog(@"Expected 1 but found %ld input source(s) with bundle id: %@", inputlist.count, bundleId);
+            if (remove) return 0;   // Nothing more to do
+            printf("[ERROR] Expected 1 but found %ld input source(s) with bundle id: %s\n", inputlist.count, [bundleId UTF8String]);
             return -1;
         }
-        status = TISEnableInputSource((__bridge TISInputSourceRef)(inputlist[0]));
-        if (status == paramErr) {
-            NSLog(@"Failed to enable input source reference: %@", inputlist[0]);
-            return -1;
+        if (remove) {
+            status = TISDisableInputSource((__bridge TISInputSourceRef)(inputlist[0]));
+            if (status == paramErr) {
+                printf("[ERROR] Failed to disable input source reference: %s\n", [inputlist[0] UTF8String]);
+                return -1;
+            }
         }
-        status = TISSelectInputSource((__bridge TISInputSourceRef)(inputlist[0]));
-        if (status == paramErr) {
-            NSLog(@"Failed to select input source reference: %@", inputlist[0]);
-            return -1;
+        if (enable) {
+            status = TISEnableInputSource((__bridge TISInputSourceRef)(inputlist[0]));
+            if (status == paramErr) {
+                printf("[ERROR] Failed to enable input source reference: %s\n", [inputlist[0] UTF8String]);
+                return -1;
+            }
+        }
+        if (select) {
+            status = TISSelectInputSource((__bridge TISInputSourceRef)(inputlist[0]));
+            if (status == paramErr) {
+                printf("[ERROR] Failed to select input source reference: %s\n", [inputlist[0] UTF8String]);
+                return -1;
+            }
         }
     }
     return 0;
