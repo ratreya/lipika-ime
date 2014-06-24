3b33edda40c21b6cf3833cdd9850033e8c213633
Ranganath Atreya <daivajnanam@gmail.com>
2014-06-24 12:22:33 -0700

    [Fixed bug in reverse mapping]

--- InputSource/Tests/DJFileConversionTest.m
+++ InputSource/Tests/DJFileConversionTest.m
@@ -9,9 +9,18 @@
 
 #import <XCTest/XCTest.h>
 #import "DJConversionController.h"
+#import "DJGoogleSchemeFactory.h"
 
 @interface DJConversionController (test)
 -(void)convertFileFromPath:(NSString *)fromPath toPath:(NSString *)toPath withEngine:(DJStringBufferManager *)engine isReverseMapping:(BOOL)isReverseMapping;
+@end
+
+@interface DJStringBufferManager (test)
+-(id)initWithEngine:(DJInputMethodEngine *)myEngine;
+@end
+
+@interface DJInputMethodEngine (test)
+-(id)initWithScheme:(id<DJInputMethodScheme>)inputScheme;
 @end
 
 @interface DJFileConversionTest : XCTestCase
@@ -20,7 +29,7 @@
 
 @implementation DJFileConversionTest
 
-- (void)testForwardConversion {
+- (void)testLipikaForwardConversion {
     NSString *outputFile = @"/tmp/DhatupaaTaSvara.itrans.out";
     DJStringBufferManager *engine = [[DJStringBufferManager alloc] init];
     [engine changeToSchemeWithName:@"ITRANS" forScript:@"Devanagari" type:DJ_LIPIKA];
@@ -33,7 +42,7 @@
     XCTAssertEqualObjects(lines[0], @"॥अथ॑ पाणिनीयधातुपा`ठः॥");
 }
 
--(void)testReverseConversion {
+-(void)testLipikaReverseConversion {
     NSString *outputFile = @"/tmp/DhatupaaTaSvara.itrans";
     DJStringBufferManager *engine = [[DJStringBufferManager alloc] init];
     [engine changeToSchemeWithName:@"ITRANS" forScript:@"Devanagari" type:DJ_LIPIKA];
@@ -44,6 +53,40 @@
     NSArray *lines = [contents componentsSeparatedByString:@"\r"];
     XCTAssertEqual(lines.count, 2427);
     XCTAssertEqualObjects(lines[0], @"||atha' paaNiniiyadhaatupaa_ThaH||");
+}
+
+-(void)testGoogleForwardConversion {
+    NSString *outputFile = @"/tmp/DhatupaaTaSvara.itrans.out";
+    DJGoogleInputScheme *scheme = [DJGoogleSchemeFactory inputSchemeForSchemeFile:@"./InputSource/Tests/Schemes/SA_ITRANS.scm"];
+    DJStringBufferManager *engine = [[DJStringBufferManager alloc] initWithEngine:[[DJInputMethodEngine alloc] initWithScheme:scheme]];
+    DJConversionController *controller = [[DJConversionController alloc] init];
+    [controller convertFileFromPath:@"./InputSource/Tests/Control/DhatupaaTaSvara.itrans" toPath:outputFile withEngine:engine isReverseMapping:NO];
+    XCTAssertTrue([[NSFileManager defaultManager] fileExistsAtPath:outputFile isDirectory:NO]);
+    NSString *contents = [NSString stringWithContentsOfFile:outputFile encoding:NSUTF8StringEncoding error:nil];
+    NSArray *lines = [contents componentsSeparatedByString:@"\r"];
+    XCTAssertEqual(lines.count, 2427);
+    XCTAssertEqualObjects(lines[0], @"॥अथ॑ पाणिनीयधातुपा॒ठः॥");
+}
+
+-(void)testGoogleReverseConversion {
+    NSString *outputFile = @"/tmp/DhatupaaTaSvara.itrans";
+    DJGoogleInputScheme *scheme = [DJGoogleSchemeFactory inputSchemeForSchemeFile:@"./InputSource/Tests/Schemes/SA_ITRANS.scm"];
+    DJStringBufferManager *engine = [[DJStringBufferManager alloc] initWithEngine:[[DJInputMethodEngine alloc] initWithScheme:scheme]];
+    DJConversionController *controller = [[DJConversionController alloc] init];
+    [controller convertFileFromPath:@"./InputSource/Tests/Control/DhatupaaTaSvara.txt" toPath:outputFile withEngine:engine isReverseMapping:YES];
+    XCTAssertTrue([[NSFileManager defaultManager] fileExistsAtPath:outputFile isDirectory:NO]);
+    NSString *contents = [NSString stringWithContentsOfFile:outputFile encoding:NSUTF8StringEncoding error:nil];
+    NSArray *lines = [contents componentsSeparatedByString:@"\r"];
+    XCTAssertEqual(lines.count, 2427);
+    XCTAssertEqualObjects(lines[0], @"||atha\\' paaNiniiyadhaatupaa\\_ThaH||");
+}
+
+-(void)testReverseMappingOverrite {
+    DJGoogleInputScheme *scheme = [DJGoogleSchemeFactory inputSchemeForSchemeFile:@"./InputSource/Tests/Schemes/SA_ITRANS.scm"];
+    DJStringBufferManager *engine = [[DJStringBufferManager alloc] initWithEngine:[[DJInputMethodEngine alloc] initWithScheme:scheme]];
+    DJSimpleReverseMapping *mappings = [engine reverseMappings];
+    DJParseOutput *output = [mappings inputForOutput:@"हर्षे"];
+    XCTAssertEqualObjects(output.input, @"She");
 }
 
 @end
