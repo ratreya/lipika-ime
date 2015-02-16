8e32ee6bd89c29720568e02ed710e33a19416aa9
Ranganath Atreya <daivajnanam@gmail.com>
2015-02-16 14:43:52 -0800

    [Fixed bug in saveSettings] [Removed batching of logs since it is no longer necessary] [Fixed code signing]

--- Common/Sources/DJLogger.m
+++ Common/Sources/DJLogger.m
@@ -7,12 +7,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#import <time.h>
 #import "DJLogger.h"
 #import "DJLipikaUserSettings.h"
-
-// Dictionary of batchId to NSArray of messages
-static NSMutableDictionary *messageMap = nil;
-static NSString *currentBatchId = nil;
 
 void logGenericBatch(enum DJLogLevel level, NSString *format, va_list variables) {
     // Don't log anything if the current level does not allow it
@@ -23,31 +20,7 @@ void logGenericBatch(enum DJLogLevel level, NSString *format, va_list variables)
     // Create the formatted log statement
     NSString *severity = [DJLipikaUserSettings logLevelStringForEnum:level];
     NSString *log = [NSString stringWithFormat:@"%@: %@", severity, [[NSString alloc] initWithFormat:format arguments:variables]];
-
-    // Local variabel to avoid concurrent modification
-    NSString *batchId = currentBatchId;
-    if (batchId) {
-        // Allocate messageMap once
-        static dispatch_once_t predicate;
-        dispatch_once(&predicate, ^{
-            messageMap = [[NSMutableDictionary alloc] initWithCapacity:0];
-        });
-
-        NSMutableArray *messages = [messageMap objectForKey:batchId];
-        if (!messages) {
-            messages = [[NSMutableArray alloc] initWithCapacity:1];
-        }
-        [messages addObject:log];
-        // Flush messages if batch is leaked
-        if (messages.count > 1000) {
-            NSLog(@"%@", [messages componentsJoinedByString:@"\n"]);
-            messages = [[NSMutableArray alloc] initWithCapacity:0];
-        }
-        [messageMap setObject:messages forKey:batchId];
-    }
-    else {
-        NSLog(@"%@", log);
-    }
+    NSLog(@"%@", log);
 }
 
 void logDebug(NSString *format, ...) {
@@ -76,25 +49,4 @@ void logFatal(NSString *format, ...) {
     va_start(args, format);
     logGenericBatch(DJ_FATAL, format, args);
     va_end(args);
-}
-
-NSString *startBatch() {
-    currentBatchId = getUUIDString();
-    return currentBatchId;
-}
-
-NSArray *endBatch(NSString *batchId) {
-    if (!messageMap || ![messageMap objectForKey:batchId]) return nil;
-    currentBatchId = nil;
-    NSArray *messages = [messageMap objectForKey:batchId];
-    [messageMap removeObjectForKey:batchId];
-    NSLog(@"%@", [messages componentsJoinedByString:@"\n"]);
-    return messages;
-}
-
-NSString *getUUIDString() {
-    CFUUIDRef theUUID = CFUUIDCreate(NULL);
-    CFStringRef string = CFUUIDCreateString(NULL, theUUID);
-    CFRelease(theUUID);
-    return (__bridge NSString *)(string);
 }
