aab985b3d41cb451a1af3b14309e83b399839d81
Ranganath Atreya <daivajnanam@gmail.com>
2017-03-19 12:20:15 -0700

    Added target for iOS

---
+++ LipikaEngine/LipikaEngine.h
@@ -0,0 +1,21 @@
+//
+//  LipikaEngine.h
+//  LipikaEngine
+//
+//  Created by Atreya Ranganath on 3/19/17.
+//  Copyright © 2017 com.daivajnanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LipikaEngine.
+FOUNDATION_EXPORT double LipikaEngineVersionNumber;
+
+//! Project version string for LipikaEngine.
+FOUNDATION_EXPORT const unsigned char LipikaEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LipikaEngine/PublicHeader.h>
+
+#import "DJStringBufferManager.h"
+#import "DJActiveBufferManager.h"
+#import "DJInputMethodEngine.h"
