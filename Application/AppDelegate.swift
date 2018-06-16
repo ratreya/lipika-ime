533885e7d1b9fc5e947731461cc6fe102df7a413
Ranganath Atreya <daivajnanam@gmail.com>
2018-06-15 22:34:37 -0700

    Empty LipikaApp target

---
+++ Application/AppDelegate.swift
@@ -0,0 +1,27 @@
+/*
+ * LipikaApp is companion application for LipikaIME.
+ * Copyright (C) 2018 Ranganath Atreya
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Application/ViewController.swift
@@ -0,0 +1,28 @@
+/*
+ * LipikaApp is companion application for LipikaIME.
+ * Copyright (C) 2018 Ranganath Atreya
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
